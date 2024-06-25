a9eaaee16afce19d7de8b3b49d2cb57cce90478a
mahdi-sahebi <m.nejadsahebi@live.co.uk>
2024-06-25 20:17:24 +0330

    [UPDATE]: Function 'get_files_list()' is written for specific file extension

--- source/app.sh
+++ source/app.sh
@@ -1,3 +1,14 @@
-files_list="$(find test/ -name "*.h")"
+# $1: Path to serch
+# $2: Files extension for searching
+function get_files_list()
+{
+	path=$1
+	extension=$2
+	
+	files_list="$(find "$path" -name "*$extension")"
+	echo "$files_list"
+}
 
+files_list="$(get_files_list "./test" ".h")"
 echo "$files_list"
+
