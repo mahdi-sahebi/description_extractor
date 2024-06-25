75de8bff0ed9905a21f0d59d6dda6752866ecc71
mahdi-sahebi <m.nejadsahebi@live.co.uk>
2024-06-25 20:42:16 +0330

    [UPDATE]: get_file_name() is define to extract file name from file path

--- source/app.sh
+++ source/app.sh
@@ -1,5 +1,6 @@
 # $1: Path to serch
 # $2: Files extension for searching
+# $?: Array of file paths
 function get_files_list()
 {
 	path=$1
@@ -9,6 +10,29 @@ function get_files_list()
 	echo "$files_list"
 }
 
-files_list="$(get_files_list "./test" ".h")"
-echo "$files_list"
+
+# $1: File path
+# $?: Pure file name without extension
+function get_file_name()
+{
+	file_path=$1
+	
+	file_name=$file_path
+	slash_pattern="/"
+	while [[ $file_name == *"/"* ]]
+	do	
+		file_name="${file_name#*$slash_pattern}"
+	done
+	
+	echo "$file_name"
+}
+
+file_path_list="$(get_files_list "./test" ".h")"
+
+for file_path in $file_path_list
+do
+	file_name="$(get_file_name "$file_path")"
+	echo "$file_name"
+done
+
 
